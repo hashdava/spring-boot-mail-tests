f45ed1c50c153e4135b3af1afa8d32576b0d839c
Afonso <afonsod@kordoba.de>
2019-05-07 16:00:19 +0100

    renamed packages!

--- src/main/java/com/hashdava/examples/springtests/EmailCfg.java
+++ src/main/java/com/hashdava/examples/springtests/EmailCfg.java
@@ -1,4 +1,4 @@
-package pt.impactzero.examples.springtests;
+package com.hashdava.examples.springtests;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
--- src/main/java/com/hashdava/examples/springtests/FeedbackController.java
+++ src/main/java/com/hashdava/examples/springtests/FeedbackController.java
@@ -1,4 +1,4 @@
-package pt.impactzero.examples.springtests;
+package com.hashdava.examples.springtests;
 
 import javax.validation.ValidationException;
 
